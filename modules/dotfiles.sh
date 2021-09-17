740cc00f59af18fe9b48d061e42e04bee6b7c677
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-09-17 07:43:38 +0200

    v0.3

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -4,16 +4,27 @@ dotFilesCopy()
 {
   # --
   # contributor:  Julio Jiménez Delgado (jouleSoft)
-  # version:      0.2
+  # version:      0.3
   # updated:      17-09-2021
+  # change:       create repo subdir if not exists at first copy
   #
   # dependencies: coreutils
   # --
 
   declare output
+  declare repo_subdir
 
   if [ ! -e "$repo/$1" ]; then
     #if there is no files yet in the repo, create the first copy of them.
+
+    #create de repo subdir if not exists
+    repo_subdir="$repo/$1"
+
+    if [ ! -e $(dirname "$repo_subdir") ]; then
+      mkdir -p "$repo_subdir"
+    fi
+
+    #make the first copy
     cp -rf "$HOME/$1" "$repo/$1" && output="${YELLOW}[   CP   ]${NC} $1"
 
   elif [ ! -e "$HOME/$1" ]; then
