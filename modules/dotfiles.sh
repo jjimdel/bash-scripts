5569bae3b44e714090b46108db1dcab90d50815a
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-09-17 07:24:12 +0200

    refactored

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -2,31 +2,34 @@
 
 dotFilesCopy()
 {
+  # --
+  # contributor:  Julio Jiménez Delgado (jouleSoft)
+  # version:      0.2
+  # updated:      17-09-2021
+  #
+  # dependencies: coreutils
+  # --
+
   declare output
-  declare -a dotFiles_array="$1"
 
-  for c in "${dotFiles_array[@]}"; do
+  if [ ! -e "$repo/$1" ]; then
+    #if there is no files yet in the repo, create the first copy of them.
+    cp -rf "$HOME/$1" "$repo/$1" && output="${YELLOW}[   CP   ]${NC} $1"
 
-    if [ ! -e "$repo/$c" ]; then
-      #if there is no files yet in the repo, create the first copy of them.
-      cp -rf "$HOME/$c" "$repo/$c" && output="${YELLOW}[   CP   ]${NC} $c"
+  elif [ ! -e "$HOME/$1" ]; then
+    #if there is no files in the homedir, copy is not needed.
+    output="${NC}[   NN   ] $1"
 
-    elif [ ! -e "$HOME/$c" ]; then
-      #if there is no files in the homedir, copy is not needed.
-      output="${NC}[   NN   ] $c"
+  elif diff -q "$HOME/$1" "$repo/$1" > /dev/null; then
+    #if there is no differences between the dotFile from source and the repo,
+    #the file or directory won't be copied
+    output="${LIGHT_GREEN}[   OK   ]${NC} $1"
 
-    elif diff -q "$HOME/$c" "$repo/$c" > /dev/null; then
-      #if there is no differences between the dotFile from source and the repo,
-      #the file or directory won't be copied
-      output="${LIGHT_GREEN}[   OK   ]${NC} $c"
+  else
+    cp -rf "$HOME/$1" "$repo/$1" && output="${YELLOW}[   CP   ]${NC} $1"
+  fi
 
-    else
-      cp -rf "$HOME/$c" "$repo/$c" && output="${YELLOW}[   CP   ]${NC} $c"
-    fi
-
-    echo -e "  $output"
-
-  done
+  echo -e "  $output"
 }
 
 dotFilesCopy_legend()
