878af578679b0676b2d963ef76389bddeee7003f
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-09-17 23:07:40 +0200

    git_SomethingToStage function created

--- modules/git.sh
+++ modules/git.sh
@@ -2,6 +2,15 @@
 
 gitCommit()
 {
+  # 
+  # contributor:  Julio Jiménez Delgado (jouleSoft)
+  # version:      0.3
+  # updated:      13-09-2021
+  # change:       text formated
+  #
+  # dependencies
+  #   - git
+  # 
 
   read -rp "Do you want to stage all files and commit (Y/n): " confirm
   echo
@@ -33,10 +42,49 @@ gitCommit()
   fi
 }
 
+git_SomethingToStage()
+{
+  #
+  # contributor:  Julio Jiménez Delgado (jouleSoft)
+  # version:      0.1
+  # updated:      17-09-2021
+  # change:       -
+  #
+  # dependencies
+  #   - git
+  #
+
+  if [ $(git status --short | wc -c) = 1 ]; then
+    echo -e "${LIGHT_GREEN}GIT status of [$(pwd)]${NC}"
+    echo " ---"
+    # check git status
+    git status --short
+    echo " ---"
+
+    echo
+
+    # (todo) Ask user if commit and push
+
+    gitCommit
+  else
+    echo -e "${LIGHT_GREEN} DotFiles up to date${NC}"
+  fi
+}
+
 gitCheck_and_commit()
 {
-  # -.- [PARAMETERS] -.-
-  # $1 - repository path
+  #
+  # contributor:  Julio Jiménez Delgado (jouleSoft)
+  # version:      0.3
+  # updated:      13-09-2021
+  # change:       text formated
+  #
+  # dependencies
+  #   - git
+  #
+  # parameters
+  #   - $1 : repository path
+  #
 
   # if repository exists
   if [ -e "$1" ]; then
@@ -52,18 +100,7 @@ gitCheck_and_commit()
 
   # change to the repo directory
   if cd "$gitRepo"; then
-    echo -e "${LIGHT_GREEN}GIT status of [$(pwd)]${NC}"
-    echo " ---"
-    # check git status
-    git status --short
-    echo " ---"
-
-    echo
-
-    # (todo) Ask user if commit and push
-
-    gitCommit
-
+    git_SomethingToStage
   else
     echo "$gitRepo doesn't exist or it can't be openned"
   fi
