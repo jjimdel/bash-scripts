26622cd09dac8f8a7615ea8b58b77ad4c8f01925
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-09-18 07:40:29 +0200

    color variables added and indent checked

--- modules/general.sh
+++ modules/general.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+#### [DECLARATIONS AND DEFINITIONS] ####
+
+#No color
+declare NC='\033[0m'
+
+#Standar colors
+declare BLACK='\033[0;30m'
+declare BLUE='\033[0;34m'
+declare CYAN='\033[0;36m'
+declare GREEN='\033[0;32m'
+declare ORANGE='\033[0;33m'
+declare PURPLE='\033[0;35m'
+declare RED='\033[0;31m'
+declare YELLOW='\033[1;33m'
+
+#Light colors
+declare LIGHT_BLUE='\033[1;34m'
+declare LIGHT_CYAN='\033[1;36m'
+declare LIGHT_GREEN='\033[1;32m'
+declare LIGHT_GREY='\033[0;37m'
+declare LIGHT_PURPLE='\033[1;35m'
+declare LIGHT_RED='\033[1;31m'
+declare LIGHT_WHITE='\033[1;37m'
+
+#Dark colors
+declare DARK_GREY='\033[1;30m'
+
+#### [FUNCTIONS] ####
+
 #Script header
 header()
 {
@@ -7,11 +36,6 @@ header()
   # $1 - script_name
   # $2 - version
   # $3 - description
-
-  #Init color variables
-  declare NC='\033[0m'
-  declare LIGHT_GREY='\033[0;37m'
-  declare YELLOW='\033[1;33m'
 
   echo
   echo -e "${LIGHT_GREY} $1 ${YELLOW}$2 ${LIGHT_GREY}- $3${NC}\n"
@@ -21,62 +45,68 @@ header()
 #Argument control
 args_check()
 {
-        #When less arguments than expected: help text is shown
-        if [ "$#" -lt "$args" ]; then
-                echo " More arguments needed."
-                echo "     Expected:   $args"
-                echo "     Intruduced: $#"
-                echo
-                echo " Syntax:"
-                echo "     $script_name ${args_array[*]}"
-                echo
-                echo " Where:"
-                for (( i=0; i<args; i++ ))
-                do
-                        echo "     ${args_array[$i]} - ${args_definition_array[$i]}"
-                done
-                echo
+  if [ "$#" -lt "$args" ]; then
+    #When less arguments than expected: help text is shown
 
-                args_check_result=1
+    echo " More arguments needed."
+    echo "     Expected:   $args"
+    echo "     Intruduced: $#"
+    echo
+    echo " Syntax:"
+    echo "     $script_name ${args_array[*]}"
+    echo
+    echo " Where:"
+    for (( i=0; i<args; i++ ))
+    do
+      echo "     ${args_array[$i]} - ${args_definition_array[$i]}"
+    done
+    echo
 
-        #When more arguments than expected: help text is shown
-        elif [ "$#" -gt "$args" ]; then
-                echo " More arguments than expected."
-                echo "     Expected:   $args"
-                echo "     Intruduced: $#"
-                echo
-                echo " Syntax:"
-                echo "     $script_name ${args_array[*]}"
-                echo
-                echo " Where:"
-                for (( i=0; i<args; i++ ))
-                do
-                        echo "     ${args_array[$i]} - ${args_definition_array[$i]}"
-                done
-                echo
+    args_check_result=1
 
-                args_check_result=1
+  elif [ "$#" -gt "$args" ]; then
+    #When more arguments than expected: help text is shown
 
-        #All arguments needed: OK.
-        else
-                args_check_result=0
-        fi
+    echo " More arguments than expected."
+    echo "     Expected:   $args"
+    echo "     Intruduced: $#"
+    echo
+    echo " Syntax:"
+    echo "     $script_name ${args_array[*]}"
+    echo
+    echo " Where:"
+    for (( i=0; i<args; i++ ))
+    do
+      echo "     ${args_array[$i]} - ${args_definition_array[$i]}"
+    done
+    echo
+
+    args_check_result=1
+
+  else
+    #All arguments needed: OK.
+
+    args_check_result=0
+  fi
 }
 
 #Dependency control
 deps_check()
 {
-        #Number of software dependencies
-        declare deps="$#"
+  #Number of software dependencies
+  declare deps="$#"
 
-        #When number of dependencies are more than zero, they will be checked
-        if [[ $deps -gt 0 ]]; then
-                for d in "$@"; do
-                        #If the dependency in $d is not installed,
-                        #it will be stored in '${deps_check_array}'
-                        if [[ $(which "$d" > "/dev/null  2>&1") -gt 0 ]]; then
-                                deps_check_array+=("$d")
-                        fi
-                done
-        fi
+  if [[ $deps -gt 0 ]]; then
+    #When number of dependencies are more than zero, they will be checked
+
+    for d in "$@"; do
+      if [[ $(which "$d" > "/dev/null  2>&1") -gt 0 ]]; then
+        #If the dependency in $d is not installed,
+        #it will be stored in '${deps_check_array}'
+
+        deps_check_array+=("$d")
+
+      fi
+    done
+  fi
 }
