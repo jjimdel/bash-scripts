9d4e01cdbd596d32312a885d402bd5792dbf06b3
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-10-12 16:25:40 +0200

    logging code exported to common.sh module

--- js-check-ip.sh
+++ js-check-ip.sh
@@ -24,73 +24,68 @@
 #   None
 #
 
-####  -.- [MODULES] -.-  ####
-. /home/jjimenez/workspace/bash-scripts/modules/common.sh
+#### [MODULES] ####
 
-####  -.- [DECLARATIONS AND DEFINITIONS] -.-  ####
+. $HOME/workspace/bash-scripts/modules/common.sh
+
+#### [DECLARATIONS AND DEFINITIONS] ####
 
 #Script info and arguments evaluation variables
-declare script_name="js-check-ip.sh"
-declare version="v.0.1"
-declare description="Checks an IP address and log the result"
+script_name="js-check-ip.sh"
+version="v0.3"
+description="Checks an IP address and log the result"
+
+#Dependencies array: used for checking the dependencies.
+#Declared in 'common.sh' module.
+deps_array=()
 
 #Arguments arrays: used on the help screen when args_check() function evals '1'.
-declare -a args_array=(
+args_array=(
   "source_addr"
   "target"
-  "log_file"
+  "log_dir"
 )
-declare -a args_definition_array=(
+
+args_definition_array=(
   "Source interface or IP address"
   "Hostname or IP address to check"
-  "Logfile path"
+  "Log directory path"
 )
 
 #Total arguments expected / introduced
-declare args=${#args_array[@]}
-
-#Dependencies array: used for checking the dependencies
-#Leave empty if there is not any dependency
-declare -a deps_array=()
-
-#Dependencies check array: used in the help screen for enumerating the missing dependencies
-#it will be completed in 'deps_check()' function
-declare -a deps_check_array=()
+args=${#args_array[@]}
 
 #Global operational variables
 # NONE
 
-#### -.- [FUNCTIONS] -.-  ####
-
-check_ip()
-{
-  declare time_stamp
-  declare log_file
-  declare log_write
-
-  time_stamp="$(date +%d-%m-%y' '%H:%M:%S' '%Z)"
-  log_file=""
-
-
-  ping -c 1 -4 -I "$1" "$2" > /dev/null
-
-  if [ "$?" -eq 0 ]; then
-    log_write="$time_stamp: ping to $2 succeeded"
-  else
-    log_write="$time_stamp: ping to $2 failed"
-  fi
-
-
-  if [ ! -e "$3" ]; then
-    echo "# js-check-ip.sh log file" > "$3"
-    echo "---" >> "$3"
-  fi
-  echo "$log_write" >> "$3"
-}
-
+#### [FUNCTIONS] ####
 
 #Operational functions (if required)
-#
+check_ip()
+{
+  # 
+  # parameters
+  #   - $1 - Source IP address or interface
+  #   - $2 - Target IP address
+  #   - $3 - Log directory
+  #
+
+  #Ping once to IPv4 address ($2) from an specific interface ($1)
+  ping -c 1 -4 -I "$1" "$2" > /dev/null
+
+  #Log string
+  if [ "$?" -eq 0 ]; then
+    log_write="ping to $2 succeeded"
+  else
+    log_write="ping to $2 failed"
+  fi
+
+  #Write log header into logfile ($3/logfile)
+  log_header "$3"
+
+  #Write to logfile ($3/logfile)
+  log_in "$log_write" "$3"
+}
 
 #Main function
 main()
@@ -99,55 +94,27 @@ main()
   echo
 }
 
-#### -.- [EXECUTION] -.-  ####
+#### [EXECUTION] ####
 
 #Printing the header
 header "$script_name" "$version" "$description"
+
+#Check if config file exists (when needed)
+# config_file_check "<config_file>"
 
 #Dependency evalutation
 deps_check "${deps_array[@]}"
 
 #Arguments number evaluation
-if [[ ${#deps_check_array[@]} -eq 0 ]]; then
-  args_check "$@"
-else
-  echo "The dependencies listed below are needed:"
-  for e in "${deps_check_array[@]}"; do
-    echo "     $e"
-  done
+args_check "$@"
 
-  echo
+#Main function execution
+main "$@"
 
-  #If there are not all the dependencies, the main function will be skipped
-  args_check_result=1
-fi
+#### [FINALIZATION] ####
 
-if [ $args_check_result -eq 0 ]; then
-  main "$@"
-
-  if [ "$?" -eq 0 ]; then
-    exit 0
-  else
-    exit 1
-  fi
-fi
-
-#### -.- [FINALIZATION] -.-  ####
-
-#Script header
-unset script_name
-unset version
-unset description
-
-#Argument evaluation
-unset args
-unset args_array
-unset args_definition_array
-unset args_check_result
-
-#Dependency evaulation
-unset deps_check_array
-unset deps_array
+#Unset common.sh module variables
+common_unset
 
 #Operational variables (if any)
 #
