e5cba8d8e0c7ee56ec21f341f3ef5a23fb3e5177
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2022-05-27 06:06:49 +0200

    Minor correction

--- js-sync-dotfiles.sh
+++ js-sync-dotfiles.sh
@@ -218,7 +218,7 @@ print_help()
   Options:
     -c          Check active dotFiles (default)
     -d          Get dotFiles differences
-    -s          Syncroize
+    -s          Syncronize
     -v          More detailed output durint sync (verbose)
                 Used with -s
 
